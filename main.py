7257244ba4aca9f1ccc7572afaa611cfd6442046
Luca <152313871+Luca2040@users.noreply.github.com>
2024-02-27 18:36:29 +0100

    upload

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from pdf_parsing import parse_pdf
 
-text_parsed = parse_pdf("file2.pdf", "Tabella")  # Save the parsed PDF in a string
-output_file = "parsedPDF.txt"
+input_file_path = "file.pdf"  # path to the PDF file to parse
+table_name = "Tabella"  # name to tell the cat that there is a table
+
+output_file = "parsedPDF.txt"  # path to the file to save
+
+text_parsed = parse_pdf(input_file_path, table_name)  # Save the parsed PDF in a string
 
 with open(output_file, "w", encoding='utf-8') as file_to_save:
     file_to_save.write(text_parsed)
