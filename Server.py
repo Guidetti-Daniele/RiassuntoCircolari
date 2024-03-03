aaf73da59059ad408c379f62fb5d8604404a55e6
Luca <152313871+Luca2040@users.noreply.github.com>
2024-03-03 17:24:54 +0100

    update

--- Server.py
+++ Server.py
@@ -53,7 +53,7 @@ def upload_file():
         return 'file parsed'
 
 
-db_path_variable = 'DB_PATH'
+db_path_variable = 'DB_PATH'  # environment variable containing db path
 
 if __name__ == '__main__':
     app.run(debug=False, host="0.0.0.0")
