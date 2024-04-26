c5b4d3fe0819971e316d36c6e377be80604f2fcb
1-23-4-56 <152313871+Luca2040@users.noreply.github.com>
2024-04-27 00:24:28 +0200

    solved permission issue in dockerfile

--- Server/flask/main.py
+++ Server/flask/main.py
@@ -2,8 +2,8 @@ import os
 from app.Parameters import parameters
 from app.Server import app
 
-parameters.init(os.getenv("PARAMETERS_PATH", "parameters/parameters.json"))
-parameters.connection_string = os.getenv("DB_PATH", "database/database.db")
+parameters.init("/app/parameters/parameters.json")
+parameters.connection_string = "/app/database/db.sqlite"
 
 if __name__ == "__main__":
     app.run()
