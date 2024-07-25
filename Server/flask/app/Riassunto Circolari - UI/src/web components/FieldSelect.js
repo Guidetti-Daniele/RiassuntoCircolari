8e3ac0338e5ea5ba88de5ee7a3b2f11286b10b76
Daniele Guidetti <danieleguidetti06@gmail.com>
2024-07-25 15:49:58 +0200

    Transformed the css for the FieldSelect component and improved the slot management

--- Server/flask/app/Riassunto Circolari - UI/src/web components/FieldSelect.js
+++ Server/flask/app/Riassunto Circolari - UI/src/web components/FieldSelect.js
@@ -10,20 +10,22 @@ export class FieldSelect extends LitElement {
       box-sizing: border-box;
       margin: 0;
       padding: 0;
+      color: var(--field-select-color);
     }
 
-    .dropdown {
+    .wrapper {
       position: relative;
       width: 100%;
       height: 100%;
     }
 
-    .dropdown-selected {
+    .current-option {
       width: 100%;
       height: 100%;
-      padding: 1.5em;
+      padding: var(--field-select-vertical-padding)
+        var(--field-select-horizontal-padding);
       border-radius: 5px;
-      background-color: var(--select-color);
+      background-color: var(--field-select-background);
       display: flex;
       justify-content: space-between;
       align-items: center;
@@ -32,8 +34,8 @@ export class FieldSelect extends LitElement {
       user-select: none;
     }
 
-    .dropdown-selected:hover {
-      background: var(--select-color-hover);
+    .current-option:hover {
+      background: var(--field-select-background-hover);
     }
 
     .arrow {
@@ -43,56 +45,67 @@ export class FieldSelect extends LitElement {
       transition: rotate 0.5s;
     }
 
-    .dropdown-menu {
+    .menu {
       position: absolute;
-      top: 50px;
-      left: 0px;
+      transform-origin: top;
+      transform: translateY(-50%);
       width: 100%;
       border-radius: 5px;
-      background: var(--select-color);
-      transform: translateY(-50px);
+      overflow-y: auto;
+      background: var(--field-select-background);
+      box-shadow: 0 4px 8px 1px black;
       opacity: 0;
       visibility: hidden;
       transition: transform, opacity, 0.5s;
       z-index: 10;
     }
 
+    .menu::-webkit-scrollbar {
+      width: var(--field-select-scrollbar-width);
+    }
+
+    .menu::-webkit-scrollbar-thumb {
+      background: var(--field-select-scrollbar-thumb-background);
+      border-radius: var(--field-select-scrollbar-border-radius);
+    }
+
+    .menu::-webkit-scrollbar-track {
+      background: var(--field-select-scrollbar-track-background);
+    }
+
     ::slotted(option) {
-      height: 30px;
-      padding: 1.2em 0.8em;
       display: flex;
       align-items: center;
-      cursor: pointer;
+      min-height: var(--field-select-option-min-height);
+      padding: var(--field-select-option-vertical-padding)
+        var(--field-select-option-horizontal-padding) !important;
+      color: var(--field-select-color) !important;
       transition: background-color 0.5s;
+      cursor: pointer;
       user-select: none;
     }
 
-    ::slotted(option):hover {
-      background-color: rgba(63, 63, 63, 0.747);
+    ::slotted(option:hover) {
+      background-color: var(--field-select-option-hover);
     }
 
-    .dropdown-menu:has(::slotted(option):nth-child(8)) {
-      max-height: 700%;
-      overflow-y: scroll;
-    }
-
-    .dropdown .dropdown-menu {
+    .menu {
       transform: translateY(0px);
       opacity: 1;
       visibility: visible;
-      display: block;
     }
 
-    .dropdown .dropdown-selected {
+    .current-option {
       border: 1px solid var(--green);
     }
 
-    .dropdown .dropdown-selected .arrow {
+    .arrow {
       rotate: 180deg;
     }
   `;
 
   static properties = {
+    maxElementsInView: {},
     name: {},
     value: {},
   };
@@ -100,38 +113,60 @@ export class FieldSelect extends LitElement {
   constructor() {
     super();
 
-    this.name = "";
-    this.value = "";
+    this.updatedSlot = false; // This flag is used to avoid to run the handleSlotChange method twice
+    this.maxElementsInView = 7; // After this number of options will be compare a scrollbar
+    this.name = ""; // name of the field
+    this.value = ""; // the current of the selected option
   }
 
-  // Every slotted element which isn't an option will be removed
-  firstUpdated() {
+  handleSlotChange() {
+    if (this.updatedSlot) {
+      this.updatedSlot = false;
+      return;
+    }
+
+    // Every slotted element which isn't an option will be removed
+    const host = this.shadowRoot.host;
     const slot = this.renderRoot?.querySelector("slot");
     const slottedElements = slot.assignedElements();
 
     Array.from(slottedElements).forEach((slotted, index) => {
       if (slotted.tagName !== "OPTION") {
-        const host = this.shadowRoot.host;
         const child = host.children[index];
 
+        // N.B: this operation causes the event @slotchange to be fired
+        // so I set the flag updatedSlot to true to avoid to make this method running again.
         host.removeChild(child);
       }
     });
+
+    this.updatedSlot = true;
+
+    // Setting the max-height of the menu according to maxElementsInView
+    const maxHeight = Array.from(host.children)
+      .slice(-this.maxElementsInView)
+      .reduce((height, currentChild) => {
+        const childHeight = currentChild.getBoundingClientRect().height;
+        return height + childHeight;
+      }, 0);
+
+    const menuElement = this.renderRoot?.querySelector(".menu");
+    menuElement.style.maxHeight = `${maxHeight}px`;
   }
 
   render() {
     return this.name
       ? html`
-          <div class="dropdown">
-            <div class="dropdown-selected">
-              <span class="selected-option-text"
-                >${this.value || this.name}</span
-              >
+          <div class="wrapper">
+            <div class="current-option">
+              <span>${this.value || this.name}</span>
               <div class="arrow"></div>
             </div>
 
-            <div class="dropdown-menu">
-              <slot> Error no option provided! </slot>
+            <div class="menu">
+              <slot @slotchange=${this.handleSlotChange}>
+                Error no option provided!
+              </slot>
             </div>
           </div>
         `
